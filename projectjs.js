aa6dc3ee43c7fe802633afc36c51db0e57ac765f
rawan1231 <132661879+rawan1231@users.noreply.github.com>
2023-05-05 13:48:05 +0300

    Add files via upload

---
+++ projectjs.js
@@ -0,0 +1,6 @@
+let btn = document.getElementById('btn');
+let light = document.getElementById('light');
+function togglebtn(){
+    btn.classList.toggle("active");
+    light.classList.toggle("on");
+}
